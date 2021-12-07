c74c6ac890508a993a67a01e49b33c6a61b0a97e
Fnwrkio <firmansyah.assignment@gmail.com>
2021-12-07 14:37:21 +0700

    Single Page Application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
@@ -25,11 +24,6 @@ class App extends Component {
   }
   render() {
     return (
-      // className to distinguish between class in js and class in html
-      // {} is javascript expression
-      // this.state.string is a variable
-      // You cannot modified state without using this keyword
-      // component will be rerendered when state is changing
       <div className="App">
         { this.state.monsters.map(monster => (
           <h1 key={monster.id}>{ monster.name }</h1>
